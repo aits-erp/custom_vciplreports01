b002981b5cd0fd65c0fa46671361bf000d3ae969
saimore1234 <moresai899@gmail.com>
2025-12-09 12:08:23 +0530

    Added Distributor Report1

--- vciplreports/vciplreports/report/distributor_report/distributor_report.js
+++ vciplreports/vciplreports/report/distributor_report/distributor_report.js
@@ -3,28 +3,12 @@ frappe.query_reports["Distributor Report"] = {
         {
             fieldname: "from_date",
             label: __("From Date"),
-            fieldtype: "Date",
-            reqd: 0
+            fieldtype: "Date"
         },
         {
             fieldname: "to_date",
             label: __("To Date"),
-            fieldtype: "Date",
-            reqd: 0
-        },
-        {
-            fieldname: "asm",
-            label: __("ASM"),
-            fieldtype: "Link",
-            options: "Sales Person",
-            reqd: 0
-        },
-        {
-            fieldname: "rsm",
-            label: __("RSM"),
-            fieldtype: "Link",
-            options: "Sales Person",
-            reqd: 0
+            fieldtype: "Date"
         }
     ]
 };
--- vciplreports/vciplreports/report/distributor_report/distributor_report.py
+++ vciplreports/vciplreports/report/distributor_report/distributor_report.py
@@ -7,15 +7,12 @@ def execute(filters=None):
 def get_columns():
     return [
         {"label": "Customer Group", "fieldname": "customer_group", "fieldtype": "Data", "width": 120},
-        {"label": "Distributor Name", "fieldname": "customer_name", "fieldtype": "Data", "width": 160},
-        {"label": "ASM", "fieldname": "asm", "fieldtype": "Data", "width": 140},
-        {"label": "RSM", "fieldname": "rsm", "fieldtype": "Data", "width": 140},
-        {"label": "Invoice", "fieldname": "invoice", "fieldtype": "Link", "options": "Sales Invoice", "width": 120},
-        {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 100},
-        {"label": "Due Date", "fieldname": "due_date", "fieldtype": "Date", "width": 100},
-        {"label": "Overdue Days", "fieldname": "overdue_days", "fieldtype": "Int", "width": 90},
-        {"label": "Outstanding Amount", "fieldname": "outstanding_amount", "fieldtype": "Currency", "width": 130},
-        {"label": "Total Overdue", "fieldname": "total_overdue", "fieldtype": "Currency", "width": 130},
+        {"label": "Distributor Name", "fieldname": "customer_name", "fieldtype": "Data", "width": 180},
+        {"label": "ASM", "fieldname": "asm", "fieldtype": "Data", "width": 150},
+        {"label": "RSM", "fieldname": "rsm", "fieldtype": "Data", "width": 150},
+        {"label": "Overdue Days", "fieldname": "overdue_days", "fieldtype": "Int", "width": 100},
+        {"label": "Outstanding Amount", "fieldname": "outstanding_amount", "fieldtype": "Currency", "width": 140},
+        {"label": "Total Overdue", "fieldname": "total_overdue", "fieldtype": "Currency", "width": 140},
     ]
 
 
@@ -27,12 +24,9 @@ def get_data(filters):
             si.customer,
             si.customer_name,
             si.customer_group,
-            si.posting_date,
             si.due_date,
-            si.outstanding_amount,
-            ste.sales_person
+            si.outstanding_amount
         FROM `tabSales Invoice` si
-        LEFT JOIN `tabSales Team` ste ON ste.parent = si.name
         WHERE si.docstatus = 1
           AND si.customer_group = 'Distributor'
     """, as_dict=True)
@@ -41,19 +35,22 @@ def get_data(filters):
 
     for inv in invoices:
 
+        # Fetch Sales Person assigned to Customer (TSO)
+        tso = frappe.db.get_value(
+            "Sales Team",
+            {"parent": inv.customer, "parenttype": "Customer"},
+            "sales_person"
+        )
+
         asm = rsm = None
 
-        # Step 1: Sales Person (TSO)
-        sales_person = inv.sales_person
+        if tso:
+            # Parent of TSO = ASM
+            asm = frappe.db.get_value("Sales Person", tso, "parent_sales_person")
 
-        # Step 2: Parent of Sales Person = ASM
-        parent1 = frappe.db.get_value("Sales Person", sales_person, "parent_sales_person")
-
-        # Step 3: Parent of ASM = RSM
-        parent2 = frappe.db.get_value("Sales Person", parent1, "parent_sales_person")
-
-        asm = parent1
-        rsm = parent2
+            # Parent of ASM = RSM
+            if asm:
+                rsm = frappe.db.get_value("Sales Person", asm, "parent_sales_person")
 
         # Overdue calculation
         overdue_days = 0
@@ -68,9 +65,6 @@ def get_data(filters):
             "customer_name": inv.customer_name,
             "asm": asm,
             "rsm": rsm,
-            "invoice": inv.invoice,
-            "posting_date": inv.posting_date,
-            "due_date": inv.due_date,
             "overdue_days": overdue_days,
             "outstanding_amount": inv.outstanding_amount,
             "total_overdue": total_overdue
